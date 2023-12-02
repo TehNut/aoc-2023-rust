beea1359d6fa4233146d5bf1430e866b0179870b
Nick Ignoffo <nick@tehnut.info>
2023-12-02 09:35:56 -0800

    Format day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -9,8 +9,12 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .filter(|c| c.is_numeric())
                 .map(|c| c.to_string())
                 .collect::<Vec<String>>();
-    
-            format!("{}{}", digits_in_line.first().unwrap(), digits_in_line.last().unwrap())
+
+            format!(
+                "{}{}",
+                digits_in_line.first().unwrap(),
+                digits_in_line.last().unwrap()
+            )
         })
         .map(|digit| digit.parse::<u32>().unwrap())
         .collect::<Vec<u32>>();
@@ -19,32 +23,43 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let string_digits: Vec<&str> = Vec::from(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]);
+    let string_digits: Vec<&str> = Vec::from([
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]);
 
-    let values = input.lines().map(|line| {
-        let mut digit_locations: Vec<(String, usize)> = Vec::new();
-        
-        // First get the locations of each numeric character
-        line.chars()
-            .enumerate()
-            .filter(|(_, c)| c.is_numeric())
-            .for_each(|(idx, c)| digit_locations.push((c.to_owned().to_string(), idx)));
+    let values = input
+        .lines()
+        .map(|line| {
+            let mut digit_locations: Vec<(String, usize)> = Vec::new();
 
-        // Next get the locations of each word digit
-        string_digits.iter()
-            .map(|d| (d, line.match_indices(d)))
-            .for_each(|(d, matches)| {
-                matches.for_each(|(pos, _)| {
-                    let digit_index = string_digits.iter().position(|e| e.eq(d)).unwrap();
-                    digit_locations.push((format!("{}", digit_index + 1), pos))
-                })
-            });
+            // First get the locations of each numeric character
+            line.chars()
+                .enumerate()
+                .filter(|(_, c)| c.is_numeric())
+                .for_each(|(idx, c)| digit_locations.push((c.to_owned().to_string(), idx)));
 
-        let first_digit = &digit_locations.iter().min_by(|(_, pos1), (_, pos2)| pos1.cmp(pos2));
-        let last_digit = &digit_locations.iter().max_by(|(_, pos1), (_, pos2)| pos1.cmp(pos2));
+            // Next get the locations of each word digit
+            string_digits
+                .iter()
+                .map(|d| (d, line.match_indices(d)))
+                .for_each(|(d, matches)| {
+                    matches.for_each(|(pos, _)| {
+                        let digit_index = string_digits.iter().position(|e| e.eq(d)).unwrap();
+                        digit_locations.push((format!("{}", digit_index + 1), pos))
+                    })
+                });
 
-        format!("{}{}", first_digit.unwrap().0, last_digit.unwrap().0)
-    }).map(|digit| digit.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+            let first_digit = &digit_locations
+                .iter()
+                .min_by(|(_, pos1), (_, pos2)| pos1.cmp(pos2));
+            let last_digit = &digit_locations
+                .iter()
+                .max_by(|(_, pos1), (_, pos2)| pos1.cmp(pos2));
+
+            format!("{}{}", first_digit.unwrap().0, last_digit.unwrap().0)
+        })
+        .map(|digit| digit.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
 
     Some(values.iter().sum())
 }
@@ -61,13 +76,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(142));
     }
 
     #[test]
     fn test_part_two() {
-        let result: Option<u32> = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result: Option<u32> = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(281));
     }
 }
